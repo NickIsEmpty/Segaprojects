76c0f35cb1162ed99ea2b258987b5ae6294bdbb9
NickIsEmpty <119450823+NickIsEmpty@users.noreply.github.com>
2023-01-06 19:49:42 +0200

    Add files via upload

---
+++ Sega1stGame/out/res/resources.d
@@ -0,0 +1,3 @@
+out/res/resources.o: res/resources.res \
+C:/Users/Goncharov/Desktop/Sega1stGame/res/Room1AaBb.png \
+C:/Users/Goncharov/Desktop/Sega1stGame/res/Walk-Animations.png
---
+++ Sega1stGame/res/resources.res
@@ -0,0 +1,2 @@
+SPRITE SoldWalk "Walk-Animations.png" 4 4 FAST 15
+IMAGE LVL "Room1AaBb.png" BEST ALL 
