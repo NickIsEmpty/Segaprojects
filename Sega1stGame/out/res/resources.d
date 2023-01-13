72f4fc77ee6d1ee696daef34a4d28cbe26d2688f
NickIsEmpty <119450823+NickIsEmpty@users.noreply.github.com>
2023-01-13 20:41:46 +0200

    Add files via upload

---
+++ Sega1stGame/out/res/resources.d
@@ -0,0 +1,4 @@
+out/res/resources.o: res/resources.res \
+C:/Users/Goncharov/Desktop/Sega1stGame/res/imgbullet.bmp \
+C:/Users/Goncharov/Desktop/Sega1stGame/res/Sold.png \
+C:/Users/Goncharov/Desktop/Sega1stGame/res/Tiles.png
---
+++ Sega1stGame/res/resources.res
@@ -0,0 +1,3 @@
+IMAGE LVL "Tiles.png" 0  
+SPRITE  Soldier "Sold.png" 4 4 FAST 16
+SPRITE Bullet "imgbullet.bmp" 1 1 FAST 0
