0307673c1b01b1f482c3060772aa57b908aa9932
NickIsEmpty <119450823+NickIsEmpty@users.noreply.github.com>
2023-01-31 22:40:43 +0200

    Delete Sega1stGame directory

--- Sega1stGame/out/res/resources.d
+++
@@ -1,4 +0,0 @@
-out/res/resources.o: res/resources.res \
-C:/Users/Goncharov/Desktop/Sega1stGame/res/Room1A.png \
-C:/Users/Goncharov/Desktop/Sega1stGame/res/imgbullet.bmp \
-C:/Users/Goncharov/Desktop/Sega1stGame/res/Sold.png
--- Sega1stGame/res/resources.h
+++
@@ -1,8 +0,0 @@
-#ifndef _RES_RESOURCES_H_
-#define _RES_RESOURCES_H_
-
-extern const Image LVL;
-extern const SpriteDefinition Soldier;
-extern const SpriteDefinition Bullet;
-
-#endif // _RES_RESOURCES_H_
