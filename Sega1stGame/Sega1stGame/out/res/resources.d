9ecd0b5a62504b9e37ba8d2ebfd277b66033f875
NickIsEmpty <119450823+NickIsEmpty@users.noreply.github.com>
2023-02-07 18:42:50 +0200

    Add files via upload

---
+++ Sega1stGame/Sega1stGame/out/res/resources.d
@@ -0,0 +1,5 @@
+out/res/resources.o: res/resources.res \
+C:/Users/Goncharov/Desktop/Sega1stGame/res/imgbullet.bmp \
+C:/Users/Goncharov/Desktop/Sega1stGame/res/Sold.png \
+C:/Users/Goncharov/Desktop/Sega1stGame/res/Room1AaB.png \
+C:/Users/Goncharov/Desktop/Sega1stGame/res/Xenom.png
---
+++ Sega1stGame/Sega1stGame/res/resources.res
@@ -0,0 +1,4 @@
+IMAGE LVL "Room1AaB.png" 0  
+SPRITE  Soldier "Sold.png" 4 4 FAST 5
+SPRITE  Xenom "Xenom.png" 4 4 FAST 5
+SPRITE Bullet "imgbullet.bmp" 1 1 FAST 0
