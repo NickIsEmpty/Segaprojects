b616dd7cd82abeb439339cd65e4ee1da6222ef95
NickIsEmpty <119450823+NickIsEmpty@users.noreply.github.com>
2023-02-27 23:28:59 +0200

    Add files via upload

---
+++ Test/out/res/resources.d
@@ -0,0 +1,6 @@
+out/res/resources.o: res/resources.res \
+C:/Users/Goncharov/Desktop/Test/res/Levels/Room2.png \
+C:/Users/Goncharov/Desktop/Test/res/Levels/Room1.png \
+C:/Users/Goncharov/Desktop/Test/res/sprites/Xenom.png \
+C:/Users/Goncharov/Desktop/Test/res/tiles/imgbullet.bmp \
+C:/Users/Goncharov/Desktop/Test/res/sprites/Sold.png
