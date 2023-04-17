43a4f4fd2cfe4b167d9efbb944b514a8ad7e8624
NickIsEmpty <119450823+NickIsEmpty@users.noreply.github.com>
2023-04-17 21:50:44 +0300

    Delete Test directory

--- Test/out/res/resources.d
+++
@@ -1,6 +0,0 @@
-out/res/resources.o: res/resources.res \
-C:/Users/Goncharov/Desktop/SegaProjects/Test/res/sprites/Xenom.png \
-C:/Users/Goncharov/Desktop/SegaProjects/Test/res/Levels/Room1.png \
-C:/Users/Goncharov/Desktop/SegaProjects/Test/res/tiles/imgbullet.bmp \
-C:/Users/Goncharov/Desktop/SegaProjects/Test/res/Levels/Room2.png \
-C:/Users/Goncharov/Desktop/SegaProjects/Test/res/sprites/Sold.png
