4089682e54b31e67d191dd0c437b26030bbf0813
NickIsEmpty <119450823+NickIsEmpty@users.noreply.github.com>
2023-05-08 14:56:38 +0300

    Delete NewTest directory

--- NewTest/out/res/resources.d
+++
@@ -1,9 +0,0 @@
-out/res/resources.o: res/resources.res \
-C:/Users/Goncharov/Desktop/NewTest/res/sprites/Xenom.png \
-C:/Users/Goncharov/Desktop/NewTest/res/sprites/LizardBullet.png \
-C:/Users/Goncharov/Desktop/NewTest/res/Levels/Room2.png \
-C:/Users/Goncharov/Desktop/NewTest/res/tiles/imgbullet.bmp \
-C:/Users/Goncharov/Desktop/NewTest/res/Levels/Room1.png \
-C:/Users/Goncharov/Desktop/NewTest/res/Levels/Room3.png \
-C:/Users/Goncharov/Desktop/NewTest/res/sprites/Sold.png \
-C:/Users/Goncharov/Desktop/NewTest/res/sprites/LIZARD.png
