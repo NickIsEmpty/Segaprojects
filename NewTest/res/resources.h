6a644230ceb5ec814cd2c595044d87c94a7c1c1b
NickIsEmpty <119450823+NickIsEmpty@users.noreply.github.com>
2023-05-08 15:00:57 +0300

    Add files via upload

---
+++ NewTest/res/resources.h
@@ -0,0 +1,14 @@
+#ifndef _RES_RESOURCES_H_
+#define _RES_RESOURCES_H_
+
+extern const Image LVL;
+extern const Image LVL1;
+extern const Image LVL2;
+extern const SpriteDefinition Soldier;
+extern const SpriteDefinition Xenom;
+extern const SpriteDefinition Lizard;
+extern const SpriteDefinition Bullet;
+extern const SpriteDefinition LizardBullet;
+extern const Image HPoint;
+
+#endif // _RES_RESOURCES_H_
---
+++ NewTest/res/resources.res
@@ -0,0 +1,9 @@
+IMAGE  LVL "Levels/Room1.png" 0  
+IMAGE  LVL1 "Levels/Room2.png" 0
+IMAGE  LVL2 "Levels/Room3.png" 0
+SPRITE Soldier "sprites/Sold.png" 4 4 FAST 5
+SPRITE Xenom "sprites/Xenom.png" 4 4 FAST 5
+SPRITE Lizard "sprites/LIZARD.png" 4 4 FAST 5
+SPRITE Bullet "tiles/imgbullet.bmp" 1 1 FAST 0
+SPRITE LizardBullet "sprites/LizardBullet.png" 1 1 FAST 0
+IMAGE  HPoint "sprites/Health.png" 0
